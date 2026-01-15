1168a5cc5651ae9e4641e5a699a9f1d3896c7f3c
teacher-pt <aditeacherpt@gmail.com>
2026-01-16 00:44:17 +0200

    feat: add event creation functionality and improve event display in Day component

--- src/features/calendar/Day.jsx
+++ src/features/calendar/Day.jsx
@@ -1,17 +1,34 @@
+import { useDispatch } from "react-redux";
+
 import "./Day.css"
+import { addEvent } from "./calendarSlice";
 
 export default function Day({ date = "2020-01-01", info = { hebrew: "", events: [] } }) {
+    const dispatch = useDispatch();
+
     const showEvents = () => {
         alert(`${info.events?.join('\n') || 'אין אירועים'}`);
+    };
+
+    const onAddEvent = () => {
+        const eventName = prompt("הכנס שם אירוע:");
+        if (eventName) {
+            dispatch(addEvent({ date, event: eventName }));
+        }
     };
 
     return (
         <div key={date} className="calendar__cell" data-date={date}>
             <div className="calendar__day-number">{new Date(date).getDate()}</div>
             <div className="calendar__hebrew">{info.hebrew}</div>
-            <div className="calendar__events-count" title="הצג רשימת אירועים" onClick={showEvents}>
-                {info.events && `${info.events.length} אירועים`}
-            </div>
+            {info.events && (<>
+                <div className="calendar__events-count" title="הצג רשימת אירועים" onClick={showEvents}>
+                    {info.events.length} אירועים
+                </div>
+                <div className="calendar__add-event">
+                    <button onClick={onAddEvent}>הוסף אירוע</button>
+                </div>
+            </>)}
         </div>
     )
 }
